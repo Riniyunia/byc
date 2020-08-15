ec587cc88c375ac36bb0caf1a7ca22eab0d19fad
Riniyunia <64077593+Riniyunia@users.noreply.github.com>
2020-08-15 14:35:06 +0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import $ from "jquery";
+import "./styles/style.css";
+import "./styles/header-bar.css"
+import "./18880.jpg";
+import main from "./scripts/view/main.js";
+
+
+document.addEventListener("DOMContentLoaded", main);
